9044add3e6a3e2c2d6871708418c6e9e9a4806ab
Jean de Klerk <jeand@esisweblaptop.local>
2013-04-08 13:39:49 -0400

    Fixed path on require modules

--- library/jquery/1.9/jquery.module.js
+++ library/jquery/1.9/jquery.module.js
@@ -1,6 +1,6 @@
 define([
     // Load the original jQuery source file
-    '/require/library/jquery/1.9/jquery.min.js'
+    '/requiredemo/library/jquery/1.9/jquery.min.js'
     ], 
     function(){
         // Tell Require.js that this module returns a reference to jQuery
