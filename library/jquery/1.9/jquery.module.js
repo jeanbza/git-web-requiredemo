71f9addf60b2116fc7a3c18238f741ca8b0cb24c
Jean de Klerk <jeand@esisweblaptop.local>
2013-04-08 13:37:23 -0400

    Fixed comment indentations

--- library/jquery/1.9/jquery.module.js
+++ library/jquery/1.9/jquery.module.js
@@ -3,6 +3,6 @@ define([
     '/require/library/jquery/1.9/jquery.min.js'
     ], 
     function(){
-      // Tell Require.js that this module returns a reference to jQuery
-      return $;//return the global scope object
-  });
+        // Tell Require.js that this module returns a reference to jQuery
+        return $;//return the global scope object
+    });
